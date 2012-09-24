fe4c208a0ff69bfc6c4b2b593771da9f1be398a3
Till Hülsemann <thgithub@gmail.com>
2012-09-24 13:58:13 +0200

    don't use a single span for every field

--- buildBoard.js
+++ buildBoard.js
@@ -1,17 +1,9 @@
 buildBoard = function($) {
 	$(document).ready( function() {
-		var fieldTemplate = $(".resources .field");
 		var board = $(".board");
 		
-		var row = 0;
-		var col = 0;
-		for ( var row = 0; row < config.board.rows; row++) {
-			for ( var col = 0; col < config.board.columns; col++) {
-				fieldTemplate.clone()
-					.addClass('row' + row)
-					.addClass('col' + col)
-					.appendTo(board);
-			}
-		}
+		board.height(config.board.rows * config.board.field.height);
+		board.width(config.board.columns * config.board.field.width);
+		
 	});
 }(jQuery);
--- config.js
+++ config.js
@@ -2,5 +2,9 @@ config = {
 	board : {
 		rows : 10,
 		columns : 10,
+		field : {
+			width: 23,
+			height: 23
+		}
 	}
 }
