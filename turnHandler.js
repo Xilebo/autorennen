e90c06114b98dda40c6cdfb2f2013be3778394f3
Till Hülsemann <thgithub@gmail.com>
2012-09-24 12:33:25 +0200

    add turnhandler

---
+++ turnHandler.js
@@ -0,0 +1,9 @@
+function($) {
+	$(document).ready( function() {
+		var board = $(".board");
+		board.click(function (eventHandler) {
+			//~ var fieldX = eventHandler.offsetX / tileSizeX
+			//~ var fieldY = eventHandler.offsetY / tileSizeY
+		});
+	});
+}(jQuery);
