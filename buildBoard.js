3819b1a66ffc0c1d9921691d92ce54290b967921
Till Hülsemann <thgithub@gmail.com>
2012-09-24 13:48:03 +0200

    rename tile -> field (my bad english ;))

--- buildBoard.js
+++ buildBoard.js
@@ -1,15 +1,17 @@
 buildBoard = function($) {
 	$(document).ready( function() {
-		var tileTemplate = $(".resources .tile");
+		var fieldTemplate = $(".resources .field");
 		var board = $(".board");
 		
-		board.css("background-image", "url(" + config.board.background_image.url + ")");
+		board.css("background-image", "url("
+			+ config.board.background_image
+			+ ")");
 		
 		var row = 0;
 		var col = 0;
 		for ( var row = 0; row < config.board.rows; row++) {
 			for ( var col = 0; col < config.board.columns; col++) {
-				tileTemplate.clone()
+				fieldTemplate.clone()
 					.addClass('row' + row)
 					.addClass('col' + col)
 					.appendTo(board);
