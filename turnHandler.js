75de848b08d6f876b37a71c485ed52a03ce68fb6
Till Hülsemann <thgithub@gmail.com>
2012-10-04 13:32:33 +0200

    implement calculation, which field was clicked. Doesn't work yet.
    For some reasons event.offsetX and event.offsetY are undefined.

--- turnHandler.js
+++ turnHandler.js
@@ -1,9 +1,11 @@
-function($) {
+(function($) {
 	$(document).ready( function() {
 		var board = $(".board");
-		board.click(function (eventHandler) {
-			//~ var fieldX = eventHandler.offsetX / tileSizeX
-			//~ var fieldY = eventHandler.offsetY / tileSizeY
+		board.click(function (event) {
+			var fieldX = event.offsetX 
+				/ config.board.field.width;
+			var fieldY = event.offsetY 
+				/ config.board.field.height;
 		});
 	});
-}(jQuery);
+}) (jQuery);
