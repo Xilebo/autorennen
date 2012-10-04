edf87e0e46dfd6d58e2d6bc8ae16eb7f3a730dcb
Till Hülsemann <thgithub@gmail.com>
2012-10-04 14:28:46 +0200

    implement working calculation of the clicked field.
    This solution works with the offset of the board and the coordinates of the
    click, relative to the page. Not a clean way, but since the offset of the
    click event is undefined, it's the best solution that works.

--- turnHandler.js
+++ turnHandler.js
@@ -1,11 +1,16 @@
 (function($) {
 	$(document).ready( function() {
 		var board = $(".board");
+		var boardOffset = board.offset();
 		board.click(function (event) {
-			var fieldX = event.offsetX 
-				/ config.board.field.width;
-			var fieldY = event.offsetY 
-				/ config.board.field.height;
+			var clickedField = {
+				X : NaN,
+				Y : NaN
+			};
+			clickedField.X = Math.floor((event.pageX - boardOffset.left)
+				/ config.board.field.width);
+			clickedField.Y = Math.floor((event.pageY - boardOffset.top)
+				/ config.board.field.height);
 		});
 	});
 }) (jQuery);
